14f8284c6071cbdb1e3c323250be7842f27b9fa5
Tayorinde <102489053+Tayorinde@users.noreply.github.com>
2023-11-20 17:00:30 -0500

    Update GoldenSectionSearch.py
    successfully found how to find the derivative of the equation. would continue with the code whenever I can

--- GoldenSectionSearch.py
+++ GoldenSectionSearch.py
@@ -1,24 +1,28 @@
 import math
-print("f(x)= ")
-function = input()
-print("Interval:     NOTE!!!: seperate the intervals by space and not commas")
-interval = input()
-print("Range: ")
-theRange = input()
+from sympy import diff, symbols
+function = input("f(x)= ")
+x = symbols('x')
+f = eval(function)
+interval = input("Interval:     NOTE!!!: seperate the intervals by space and not commas")
+
+theRange = input("Range: ")
 
 
 
 #number of iterations that the GSS would run for 
-iterationNUmber = 0.61803
+p = 0.61803
 
 
   # split input into a list of strings 
 number = interval.split()
     #convert the string to integers 
-x,y = map(int, number)
+a0, b0 = map(int, number)
     #test split intervaL
 
-  
+# finding the derivative of the equation 
+def findDerivative():
+    dfdx = diff(f,x) 
+    print("differentation of f(x) = ", dfdx) 
 #function that gets range 
 def getRange():
     print("Range requested: " + theRange)
@@ -31,17 +35,23 @@ def getInterval():
 def numOfIterations():
     try:
         theRange_value = float(theRange)
-        if y-x != 0:
-            NoIterations = math.log( theRange_value/(y-x), iterationNUmber)
+        if b0-a0 != 0:
+            NoIterations = math.log( theRange_value/(b0-a0), p)
             NoIterations = round(NoIterations)
             print("your number of iterations would be ",NoIterations) 
         else: 
             print("thats not a valid one blud")
     except:
         print("enter a valid number as")
-    
+
+#creating a for loop for the number of iterations 
+a1 = a0 + p*(b0-a0)
+b1 = a0 + (1-p) *(b0 -a0)
+
+
 
 #test out range
+findDerivative()
 getRange()
 #test: interval
 getInterval()
