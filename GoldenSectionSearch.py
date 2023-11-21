b01cfd9625b6e305f9bb5506a1092255e8054314
Tayorinde <102489053+Tayorinde@users.noreply.github.com>
2023-11-21 11:48:56 -0500

    Update GoldenSectionSearch.py
    Better better comments

--- GoldenSectionSearch.py
+++ GoldenSectionSearch.py
@@ -1,101 +1,74 @@
 import math
-from sympy import diff, symbols, cos, tan,sin
+from sympy import diff, symbols
+
+# Prompt for user input and provide a note about the correct syntax
 print("[NOTE!!: 2x^2 should be put at 2*x**2, 2x^3 should be put as 2*x**4]")
 
 # Get the mathematical expression from the user
 function = input("f(x): ")
+
 # Define the variable symbolically
 x = symbols('x')
-# Convert the user input into a SymPy expression
-f = eval(function)
-dfdx = diff(f,x)
-interval = input("Interval:     NOTE!!!: seperate the intervals by space and not commas")
 
+# Convert the user input into a SymPy expression
+f = eval(function)  # Be cautious about using eval, ensure user input is safe
+dfdx = diff(f, x)
+
+# Get the interval from the user
+interval = input("Interval:     NOTE!!!: separate the intervals by space and not commas")
+
+# Get the range from the user
 theRange = input("Range: ")
 
-
-
-#number of iterations that the GSS would run for 
+# Constants for the Golden Section Search
 p = 0.38195
 otherP = 0.61803
 
-
-  # split input into a list of strings 
+# Split input into a list of strings
 number = interval.split()
-    #convert the string to integers 
+# Convert the string to integers
 firstInterval, secondInterval = map(int, number)
-    #test split intervaL
 
-#finding the differenciation of the function
-
-
-
-#function that gets range 
-#def getRange():
-#    print("Range requested: " + theRange)
-
-#function that gets the intervals 
+# Function that prints the interval
 def getInterval():
     print("start interval : ", interval)
 
-#to calculate the number of iterations
+# Function to calculate the number of iterations for Golden Section Search
 def numOfIterations():
     try:
         theRange_value = float(theRange)
-#        print("range: ", theRange_value)
-#        print("secondIn", secondInterval)
- #       print("firstint", firstInterval)
-#        print("otherP", otherP)
-        if secondInterval-firstInterval != 0:
-            NoIterations = math.log( theRange_value/(secondInterval-firstInterval), otherP)
+        if secondInterval - firstInterval != 0:
+            NoIterations = math.log(theRange_value / (secondInterval - firstInterval), otherP)
             NoIterations = round(NoIterations)
             return NoIterations
-            print("your number of iterations would be ",NoIterations) 
-        else: 
-            print("thats not a valid one blud")
-    except:
-        print("enter a valid number as")
+        else:
+            print("That's not a valid one blud")
+    except ValueError:
+        print("Enter a valid number.")
 
-
-
-
-
-#creating a function for the math function 
+# Function to calculate the math function for a given x
 def getFunction(x):
     return f.subs('x', x)
 
-#test: interval
+# Test: interval
 getInterval()
 numOfIterations()
 
-for i in range(1, numOfIterations()+ 1):
-    a= firstInterval + 0.38195 * (secondInterval - firstInterval)
+# Golden Section Search iterations
+for i in range(1, numOfIterations() + 1):
+    a = firstInterval + 0.38195 * (secondInterval - firstInterval)
     b = firstInterval + 0.61803 * (secondInterval - firstInterval)
     print("a", i, ": ", a, "      b", i, ":", b)
 
-    print("F(a",i,"): ", getFunction(a))
-    print("F(b",i,"): ", getFunction(b))
+    print("F(a", i, "): ", getFunction(a))
+    print("F(b", i, "): ", getFunction(b))
 
-    #else if 
+    # Compare function values and update the interval
     if getFunction(a) < getFunction(b):
         firstInterval = firstInterval
         secondInterval = b
-        print(i, "th interval: [", firstInterval," , ", secondInterval,"]")
+        print(i, "th interval: [", firstInterval, " , ", secondInterval, "]")
     else:
         firstInterval = a
         secondInterval = secondInterval
-        print(i, "th interval: [", firstInterval," , ", secondInterval,"]")
-        
-
-
-    
-
-
-
-#if fa1< fb1 then fb1 becomes the new b1 interval will firstInterval, b1
-#if getFunction(a1)<getFunction(b1):
-   # print("interval: [", firstInterval, ", ", secondInterval,"]")
-  #  secondInterval = b1
-
-#test out range
-#getRange()
+        print(i, "th interval: [", firstInterval, " , ", secondInterval, "]")
