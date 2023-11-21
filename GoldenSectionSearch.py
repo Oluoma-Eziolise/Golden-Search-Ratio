2a28434466831777e750aea97dd321ce996a2713
Tayorinde <102489053+Tayorinde@users.noreply.github.com>
2023-11-21 11:26:02 -0500

    Update GoldenSectionSearch.py

--- GoldenSectionSearch.py
+++ GoldenSectionSearch.py
@@ -1,8 +1,12 @@
 import math
 from sympy import diff, symbols
-function = input("f(x)= ")
+print("[NOTE!!: 2x^2 should be put at 2*x**2, 2x^3 should be put as 2*x**4]")
+#function = x^4 - 14*x^3 + 60*x^2 - 70*x
+#input("f(x): ")
+function = "x**4 - 14*x**3 +60*x**2 - 70*x"
 x = symbols('x')
 f = eval(function)
+dfdx = diff(f,x)
 interval = input("Interval:     NOTE!!!: seperate the intervals by space and not commas")
 
 theRange = input("Range: ")
@@ -10,49 +14,87 @@ theRange = input("Range: ")
 
 
 #number of iterations that the GSS would run for 
-p = 0.61803
+p = 0.38195
+otherP = 0.61803
 
 
   # split input into a list of strings 
 number = interval.split()
     #convert the string to integers 
-a0, b0 = map(int, number)
+firstInterval, secondInterval = map(int, number)
     #test split intervaL
 
-# finding the derivative of the equation 
-def findDerivative():
-    dfdx = diff(f,x) 
-    print("differentation of f(x) = ", dfdx) 
+#finding the differenciation of the function
+
+
+
 #function that gets range 
-def getRange():
-    print("Range requested: " + theRange)
+#def getRange():
+#    print("Range requested: " + theRange)
 
 #function that gets the intervals 
 def getInterval():
-    print("Interval requested: ", interval)
+    print("start interval : ", interval)
 
 #to calculate the number of iterations
 def numOfIterations():
     try:
         theRange_value = float(theRange)
-        if b0-a0 != 0:
-            NoIterations = math.log( theRange_value/(b0-a0), p)
+#        print("range: ", theRange_value)
+#        print("secondIn", secondInterval)
+ #       print("firstint", firstInterval)
+#        print("otherP", otherP)
+        if secondInterval-firstInterval != 0:
+            NoIterations = math.log( theRange_value/(secondInterval-firstInterval), otherP)
             NoIterations = round(NoIterations)
+            return NoIterations
             print("your number of iterations would be ",NoIterations) 
         else: 
             print("thats not a valid one blud")
     except:
         print("enter a valid number as")
 
-#creating a for loop for the number of iterations 
-a1 = a0 + p*(b0-a0)
-b1 = a0 + (1-p) *(b0 -a0)
 
 
 
-#test out range
-findDerivative()
-getRange()
+
+#creating a function for the math function 
+def getFunction(x):
+    function =  x**4 - 14*x**3 + 60*x**2 - 70*x
+    return function
+
 #test: interval
 getInterval()
 numOfIterations()
+
+for i in range(1, numOfIterations()+ 1):
+    a= firstInterval + 0.38195 * (secondInterval - firstInterval)
+    b = firstInterval + 0.61803 * (secondInterval - firstInterval)
+    print("a", i, ": ", a, "      b", i, ":", b)
+
+    print("F(a",i,"): ", getFunction(a))
+    print("F(b",i,"): ", getFunction(b))
+
+    #else if 
+    if getFunction(a) < getFunction(b):
+        firstInterval = firstInterval
+        secondInterval = b
+        print(i, "th interval: [", firstInterval," , ", secondInterval,"]")
+    else:
+        firstInterval = a
+        secondInterval = secondInterval
+        print(i, "th interval: [", firstInterval," , ", secondInterval,"]")
+        
+
+
+    
+
+
+
+#if fa1< fb1 then fb1 becomes the new b1 interval will firstInterval, b1
+#if getFunction(a1)<getFunction(b1):
+   # print("interval: [", firstInterval, ", ", secondInterval,"]")
+  #  secondInterval = b1
+
+#test out range
+#getRange()
